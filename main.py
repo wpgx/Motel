604f8eecdd2a868661565a9cde16da983087ff51
wpgx <donxman@gmail.com>
2026-08-01 12:37:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,61 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import PlainTextResponse, Response
-import requests, gzip, json
+import requests, gzip, json, os, time
+from io import BytesIO
 
 app = FastAPI()
+CACHE = {}
+CACHE_TIME = 300
+
+APP_URL = 'https://i.mjh.nz/SamsungTVPlus/.channels.json.gz'
+EPG_URL = 'https://i.mjh.nz/SamsungTVPlus/{region}.xml.gz'
+PLAYBACK_URL = 'https://jmp2.uk/{slug}'
+
+def get_data():
+    now = time.time()
+    if 'data' in CACHE and now - CACHE['data_time'] < CACHE_TIME:
+        return CACHE['data']
+    r = requests.get(APP_URL, timeout=30, headers={"User-Agent":"Mozilla/5.0"})
+    data = json.loads(gzip.decompress(r.content).decode('utf-8'))
+    CACHE['data'] = data
+    CACHE['data_time'] = now
+    return data
 
 @app.get("/samsung-ca.m3u")
-def samsung_m3u():
-    url = 'https://i.mjh.nz/SamsungTVPlus/.channels.json.gz'
-    r = requests.get(url, timeout=30, headers={"User-Agent":"Mozilla/5.0"})
-    raw = r.content
-    if raw[:2]==b'\x1f\x8b':
-        raw = gzip.decompress(raw)
-    data = json.loads(raw.decode('utf-8'))
-    slug_t = data.get('slug','SamsungTVPlus/{id}.m3u8')
-    ca = data['regions']['CA']['channels']
+@app.get("/playlist.m3u8")
+def playlist(request: Request, regions: str = "CA"):
+    data = get_data()
+    regions_list = [x.strip().lower() for x in regions.split(',')]
 
-    m3u = '#EXTM3U url-tvg="https://motel-r45n.onrender.com/samsung-ca.xml"\n'
-    for k in sorted(ca.keys(), key=lambda x: ca[x].get('chno',9999)):
-        ch=ca[k]
+    channels = {}
+    for reg in regions_list:
+        for k,v in data['regions'].items():
+            if k.lower() == reg or reg == 'all':
+                channels.update(v.get('channels', {}))
+
+    m3u = '#EXTM3U url-tvg="https://motel-r45n.onrender.com/samsung-ca.xml?regions=CA"\n'
+    for key in sorted(channels.keys(), key=lambda x: channels[x].get('chno', 9999)):
+        ch = channels[key]
         if ch.get('license_url'): continue
-        name = ch.get('name','').replace('"',"'").replace(',',"").strip()
+        name = ch['name']
         logo = ch.get('logo','')
-        group = ch.get('group','Samsung CA').replace('"',"'")
-        chno = str(ch.get('chno','')).strip()
-        play_url = f"https://jmp2.uk/{slug_t.format(id=k)}"
-        if chno:
-            m3u += f'#EXTINF:-1 tvg-id="samsung-{k}" tvg-logo="{logo}" group-title="{group}" tvg-chno="{chno}",{name}\n{play_url}\n'
-        else:
-            m3u += f'#EXTINF:-1 tvg-id="samsung-{k}" tvg-logo="{logo}" group-title="{group}",{name}\n{play_url}\n'
+        group = ch.get('group','Samsung')
+        chno = ch.get('chno','')
+        url = PLAYBACK_URL.format(slug=data['slug'].format(id=key))
+        chno_str = f' tvg-chno="{chno}"' if chno else ''
+        m3u += f'#EXTINF:-1 tvg-id="samsung-{key}" tvg-logo="{logo}" group-title="{group}"{chno_str},{name}\n{url}\n'
     return PlainTextResponse(m3u, media_type="text/plain")
 
 @app.get("/samsung-ca.xml")
-def samsung_xml():
-    r = requests.get('https://i.mjh.nz/SamsungTVPlus/ca.xml.gz', timeout=30, headers={"User-Agent":"Mozilla/5.0"})
-    data = r.content
-    if data[:2]==b'\x1f\x8b':
-        data = gzip.decompress(data)
-    return Response(content=data, media_type="application/xml")
+@app.get("/epg.xml")
+def epg(regions: str = "CA"):
+    url = EPG_URL.format(region=regions.split(',')[0].lower())
+    r = requests.get(url, timeout=30, headers={"User-Agent":"Mozilla/5.0"})
+    content = gzip.decompress(r.content) if r.content[:2]==b'\x1f\x8b' else r.content
+    return Response(content=content, media_type="application/xml")
 
 @app.get("/")
-def root():
-    return {"samsung": "/samsung-ca.m3u"}
+def status():
+    data = get_data()
+    html = "<h1>Samsung TV Plus - Working</h1>CA Channels: %d<br><br><a href='/samsung-ca.m3u'>/samsung-ca.m3u</a><br><a href='/samsung-ca.xml'>/samsung-ca.xml</a>" % len(data['regions'].get('CA',{}).get('channels',{}))
+    return Response(content=html, media_type="text/html")
